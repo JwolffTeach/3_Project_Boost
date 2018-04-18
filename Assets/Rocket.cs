14c62f99412b8de8cbfc63ad3b4e20069b27caa7
Jeremy Wolff <jwolff@dsdmail.net>
2018-04-17 22:24:12 -0600

    cu2_s03_using_serializefield
    Added thrust and rotation thrust variables. Serialized them.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rcsThrust = 100f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
@@ -26,18 +30,21 @@ public class Rocket : MonoBehaviour {
 
     private void Rotate() {
         rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
         rigidBody.freezeRotation = false;
     }
 
     private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) { // can thrust while rotating
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying) { // so it doesn't layer
                 audioSource.Play();
             }
