97e3405ac5b431e38b09b2fea3a1397e2498179c
Jeremy Wolff <jwolff@dsdmail.net>
2018-04-17 22:13:26 -0600

    cu2_s03_resolving_movement_bugs Fixed some movement bugs, rocket feels better.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -18,14 +20,30 @@ public class Rocket : MonoBehaviour {
 	}
 
     private void ProcessInput() {
-        if (Input.GetKey(KeyCode.Space)) { // can thrust while rotating
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
+        Thrust();
+        Rotate();
+    }
+
+    private void Rotate() {
+        rigidBody.freezeRotation = true;
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D)) {
             transform.Rotate(-Vector3.forward);
+        }
+        rigidBody.freezeRotation = false;
+    }
+
+    private void Thrust() {
+        if (Input.GetKey(KeyCode.Space)) { // can thrust while rotating
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) { // so it doesn't layer
+                audioSource.Play();
+            }
+        }
+        else {
+            audioSource.Stop();
         }
     }
 }
